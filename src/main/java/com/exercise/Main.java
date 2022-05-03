8f23170eeb9e48a8196a1331ab0019ed78918dfe
ks488 <ks488@outlook.com>
2022-05-03 17:50:52 +0100

    first commit

---
+++ src/main/java/com/exercise/Main.java
@@ -0,0 +1,11 @@
+package com.exercise;
+
+public class Main {
+
+    public static void main(String[] args) {
+        VowelCounter counter = new VowelCounter();
+        var resultVowel = counter.getVowelListWithCount(args);
+        System.out.println(resultVowel);      
+}
+}
+
---
+++ src/main/java/com/exercise/VowelCounter.java
@@ -0,0 +1,32 @@
+package com.exercise;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class VowelCounter {
+
+    public List<CountResult> getVowelListWithCount(String[] args) {
+        if (args.length >= 4) {
+            System.out.println("Extra arguments passed.");
+            return null;
+        } else {
+            List<String> argumentList = Arrays.asList(args);
+            List<CountResult> vowelCount = new ArrayList<>();
+            List<CountResult> consonantCount = new ArrayList<>();
+            argumentList.forEach(argument -> vowelCount.add(new CountResult(argument, getVowelCount(argument),getConsonantCount(argument))));
+    
+             return vowelCount;
+        }
+    
+
+    private Integer getVowelCount(String input) {
+        String[] inputChars = input.split("");
+        return (int) Arrays.stream(inputChars).filter(inputChar -> inputChar.matches("[aeiou]")).count();
+    }
+
+    private Integer getConsonantCount(String input) {
+        String[] inputChars = input.split("");
+        return (int) Arrays.stream(inputChars).filter(inputChar -> inputChar.matches("[bcdfghjklmnpqrstvwxyz]")).count();
+    }
+}
